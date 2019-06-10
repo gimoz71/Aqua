2a4e2bcce901c66cda1a69dc35009cb9bd8a633b
Gianluca Monti <gm3871@gmail.com>
2019-06-10 19:15:50 +0200

    init

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    static public int scene;
+
+
+    public void sceneChange(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void sceneIntro()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+}
---
+++ Assets/VideoController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public VideoPlayer vid;
+
+    public void Play()
+    {
+        vid.Play();
+    }
+
+    public void Stop()
+    {
+        vid.Stop();
+    }
+}
